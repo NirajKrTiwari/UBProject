78facadd79aaac5fe1240600508aef4dfa5de904
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-02-23 19:23:06 +0530

    initial commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,42 @@
 import css from '../styles/Header.module.css'
 import Image from 'next/image';
 import Logo from '../assets/Logo.png';
+import Link from 'next/link';
+import { UilShoppingBag } from '@iconscout/react-unicons';
 export default function Header()
 {
     return(
         <div className={css.header}>
-            <div className={css.logo}>
-                <Image src={Logo} alt=""/>
+        <Link href="/">
+        <div className={css.logo}>
+         <Image src={Logo} alt="" width={200} height={110}/>
+          </div>
+          </Link>
+        <ul className={css.menu}>
+        <Link href="/"><li>Home</li></Link>
+        <Link href="/#menu"><li>Menu</li></Link>
+          <li>About Us</li>
+          </ul>
+  
+          <div className={css.rightSide}>
+            <Link href='/cart'>
+            <div className={css.cart}>
+              <UilShoppingBag size={35} color="#2E2E2E"/>
+              <div className={css.badge}>2</div>
             </div>
-        </div>
+            </Link>
+  
+            {/* {Order && (
+            <Link href={`/order/${Order}`}>
+            <div className={css.cart}>
+              <UilReceipt size={35} color="#2E2E2E"/>
+              {Order !="" &&(
+                <div className={css.badge}>1</div>
+              )}
+            </div>
+            </Link>
+            )} */}
+          </div>
+      </div>
     )
 }
