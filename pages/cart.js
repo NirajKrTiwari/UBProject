fbe197949e15de814f0d77f65da7f47bcf78b703
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-02-25 01:40:02 +0530

    Update cart.js

--- pages/cart.js
+++ pages/cart.js
@@ -2,13 +2,14 @@ import Layout from "../components/Layout";
 import { urlFor } from "../lib/client";
 import css from '../styles/Cart.module.css';
 import Image from 'next/image';
-import { useStore } from "../store/store";
 import toast,{Toaster} from 'react-hot-toast';
+import { useStore } from '../store/store';
 //import { useState } from "react";
 // import OrderModal from "../components/OrderModal";
 
 export default function cart (){
-   // const CartData= useStore((state)=>state.cart);
+   const CartData= useStore((state)=>state.cart);
+   console.log(CartData);
     // const removeFood =useStore((state)=>state.removeFood);
     // // const [PaymentMethod, setPaymentMethod] = useState(null);
     // const handleRemove=(i)=>
