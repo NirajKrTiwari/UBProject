bab7e374ced3bbe117e72886db43203362e89d98
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-02-27 01:48:43 +0530

    Update cart.jsx

--- components/cart.jsx
+++ components/cart.jsx
@@ -1,11 +1,11 @@
-import {useStore} from "../store/store";
-import { useState } from 'react';
-export default function cart ()
-{
-     const items= useStore((state)=>state.cart.food.length);
-     const CartData= useStore((state)=>state.cart);
-     console.log(items);
-    return(
-    <div>{items}</div>
-    )
-}
+// import {useStore} from "../store/store";
+// import { useState } from 'react';
+// export default function cart ()
+// {
+//      const items= useStore((state)=>state.cart.food.length);
+//      const CartData= useStore((state)=>state.cart);
+//      console.log(items);
+//     return(
+//     <div>{items}</div>
+//     )
+// }
