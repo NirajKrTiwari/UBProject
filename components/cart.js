c880775d91581ba47d527423ed8c6c8815e0ec02
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-02-27 01:40:58 +0530

    Delete cart.js

--- components/cart.js
+++
@@ -1,7 +0,0 @@
-import {useStore} from "../store/store";
-export default function cart (){
-     const items= useStore((state)=>state.cart.food.length);
-     console.log(items);
-    return(
-    <div>{items}</div>
-    )}
