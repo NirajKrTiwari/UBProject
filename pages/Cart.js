3de7dec384eb8f1a7b9af0bae7341ac1cc635d40
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-03-07 23:42:55 +0530

    Update Cart.js

--- pages/Cart.js
+++ pages/Cart.js
@@ -7,7 +7,7 @@ import toast,{Toaster} from 'react-hot-toast';
 import { useState } from "react";
 import OrderModal from "../components/OrderModal";
 
-export default function cart (){
+export default function Cart (){
     const CartData= useStore((state)=>state.cart);
     const removeFood =useStore((state)=>state.removeFood);
     const [PaymentMethod, setPaymentMethod] = useState(null);
