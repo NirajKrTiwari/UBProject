341540270f9ba66d28e288ef65a0315887b94cfd
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-03-14 19:31:54 +0530

    initial commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,53 +1,62 @@
 import css from '../styles/Header.module.css'
 import Image from 'next/image';
 import Logo from '../assets/Logo.png';
+import MenuIcon from '../assets/menuIcon.png';
 import Link from 'next/link';
 import { UilShoppingBag } from '@iconscout/react-unicons';
-import {useStore} from "../store/store";
+import { useStore } from "../store/store";
 import { useEffect, useState } from 'react';
 import { UilReceipt } from '@iconscout/react-unicons';
-export default function Header()
-{
-  const items= useStore((state)=>state.cart.food.length);
-  const CartData= useStore((state)=>state.cart);
-  
+export default function Header() {
+  const items = useStore((state) => state.cart.food.length);
+  const CartData = useStore((state) => state.cart);
+
   const [Order, setOrder] = useState("")
   useEffect(() => {
-  setOrder(localStorage.getItem("order"));
-  },[])
+    setOrder(localStorage.getItem("order"));
+  }, [])
 
-    return(
-        <div className={css.header}>
-        <Link href="/">
+  return (
+    <div className={css.header}>
+      <div className={css.icon}>
+          {/* <svg viewBox="0 0 100 80" width="40" height="40">
+            <rect width="100" height="20"></rect>
+            <rect y="30" width="100" height="20"></rect>
+            <rect y="60" width="100" height="20"></rect>
+          </svg> */}
+          <Image src={MenuIcon} width={40} height={40}/>
+        </div>
+      <Link href="/">
         <div className={css.logo}>
-         <Image src={Logo} alt="" width={200} height={110}/>
-          </div>
-          </Link>
-        <ul className={css.menu}>
+          <Image src={Logo} alt="" width={200} height={110} />
+        </div>
+      </Link>
+      <ul className={css.menu}>
         <Link href="/"><li>Home</li></Link>
         <Link href="/#menu"><li>Menu</li></Link>
-          <li>About Us</li>
-          </ul>
-  
-          <div className={css.rightSide}>
-            <Link href='/Cart'>
+        <Link href="/"><li>About Us</li></Link>
+
+      </ul>
+
+      <div className={css.rightSide}>
+        <Link href='/Cart'>
+          <div className={css.cart}>
+            <UilShoppingBag size={35} color="#2E2E2E" />
+            <div className={css.badge}>{items}</div>
+          </div>
+        </Link>
+
+        {Order && (
+          <Link href={`/order/${Order}`}>
             <div className={css.cart}>
-              <UilShoppingBag size={35} color="#2E2E2E"/>
-              <div className={css.badge}>{items}</div>
-            </div>
-            </Link>
-  
-            {Order && (
-            <Link href={`/order/${Order}`}>
-            <div className={css.cart}>
-              <UilReceipt size={35} color="#2E2E2E"/>
-              {Order !="" &&(
+              <UilReceipt size={35} color="#2E2E2E" />
+              {Order != "" && (
                 <div className={css.badge}>1</div>
               )}
             </div>
-            </Link>
-            )}
-          </div>
+          </Link>
+        )}
       </div>
-    )
+    </div>
+  )
 }
