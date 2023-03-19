6f93eb2811c1d523b4ac60e21ef8faf570182a80
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-03-19 16:04:45 +0530

    initial commit

--- components/About.jsx
+++ components/About.jsx
@@ -9,7 +9,7 @@ export default function About() {
                 <h1>About</h1>
             <p>
                 Welcome to our restaurant, where we believe that great food brings people together. We are a family-owned and operated establishment that has been serving delicious meals to our community for over 6 years.<br/><br/>
-                Our chefs are passionate about creating dishes that are both flavorful and visually stunning. We use only the freshest, highest quality ingredients in all of our recipes. Whether you're in the mood for a juicy steak, a fresh salad, or a decadent dessert, we have something to please every palate.
+                Our chefs are passionate about creating dishes that are both flavorful and visually stunning. We use only the freshest, highest quality ingredients in all of our recipes. Whether you are in the mood for a juicy steak, a fresh salad, or a decadent dessert, we have something to please every palate.
                 {/* <br/><br/> */}
                 {/* At our restaurant, we also believe in providing exceptional customer service. Our friendly and knowledgeable staff will ensure that your dining experience is nothing short of extraordinary. We pride ourselves on creating a warm and inviting atmosphere where you can relax, unwind, and enjoy a memorable meal.
                 Thank you for considering our restaurant for your next dining experience. We look forward to serving you soon! */}
