0e53d2587da00378be9327292312667819d2a659
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-03-19 10:07:29 +0530

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -1,24 +1,24 @@
-// import Image from 'next/image';
-// import css from '../styles/About.module.css';
-// import AboutImage from '../assets/about.jpg';
-// export default function About() {
-//     return (
-//         <div>
-//             <h1>About</h1>
-//             <div className={css.container}>
-//                 <div className={css.content}>
-//             <p>
-//                 Welcome to our restaurant, where we believe that great food brings people together. We are a family-owned and operated establishment that has been serving delicious meals to our community for over 6 years.<br/><br/>
-//                 Our chefs are passionate about creating dishes that are both flavorful and visually stunning. We use only the freshest, highest quality ingredients in all of our recipes. Whether you're in the mood for a juicy steak, a fresh salad, or a decadent dessert, we have something to please every palate.
-//                 <br/><br/>
-//                 At our restaurant, we also believe in providing exceptional customer service. Our friendly and knowledgeable staff will ensure that your dining experience is nothing short of extraordinary. We pride ourselves on creating a warm and inviting atmosphere where you can relax, unwind, and enjoy a memorable meal.
-//                 Thank you for considering our restaurant for your next dining experience. We look forward to serving you soon!
-//             </p>
-//             </div>
-//             <div className={css.img}>
-//                 <Image src={AboutImage} alt='about' width={1000} height={1000} />
-//             </div>
-//             </div>
-//         </div>
-//     )
-// }
+import Image from 'next/image';
+import css from '../styles/About.module.css';
+import AboutImage from '../assets/about.jpg';
+export default function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            {/* <div className={css.container}>
+                <div className={css.content}>
+            <p>
+                Welcome to our restaurant, where we believe that great food brings people together. We are a family-owned and operated establishment that has been serving delicious meals to our community for over 6 years.<br/><br/>
+                Our chefs are passionate about creating dishes that are both flavorful and visually stunning. We use only the freshest, highest quality ingredients in all of our recipes. Whether you're in the mood for a juicy steak, a fresh salad, or a decadent dessert, we have something to please every palate.
+                <br/><br/>
+                At our restaurant, we also believe in providing exceptional customer service. Our friendly and knowledgeable staff will ensure that your dining experience is nothing short of extraordinary. We pride ourselves on creating a warm and inviting atmosphere where you can relax, unwind, and enjoy a memorable meal.
+                Thank you for considering our restaurant for your next dining experience. We look forward to serving you soon!
+            </p>
+            </div>
+            <div className={css.img}>
+                <Image src={AboutImage} alt='about' width={1000} height={1000} />
+            </div>
+            </div> */}
+        </div>
+    )
+}
