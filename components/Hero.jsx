ce342ee34951b2cfb5528846789f58b7647120af
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-03-23 14:48:55 +0530

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -14,9 +14,9 @@ export default function Hero() {
       {/* left */}
       <div className={css.left}>
       <div className={css.cherryDiv}>
-      <a target="_blank" rel="" href="https://www.google.com/maps/place/%22UNDER+BELLY%22+Food+Court+@+VIT/@23.0774289,76.8505799,15z/data=!4m6!3m5!1s0x397ce925786259b3:0xe42954da017e0246!8m2!3d23.0774289!4d76.8505799!16s%2Fg%2F11fjs9bw9t">
-          <span>VIT Bhopal</span>
-          </a>
+      <Link href="https://www.google.com/maps/place/%22UNDER+BELLY%22+Food+Court+@+VIT/@23.0774289,76.8505799,15z/data=!4m6!3m5!1s0x397ce925786259b3:0xe42954da017e0246!8m2!3d23.0774289!4d76.8505799!16s%2Fg%2F11fjs9bw9t">
+          <a target="_blank"><span>VIT Bhopal</span></a>
+          </Link>
           <span className={css.cherryIcon}>
           <Image src={location} alt="" width={40} height={30} />
           </span>
