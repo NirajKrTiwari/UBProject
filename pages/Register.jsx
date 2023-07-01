056955ae27e3af5ea0bc17c4751f8ada05318b8a
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-07-01 11:56:01 +0530

    initial commit

--- pages/Register.jsx
+++ pages/Register.jsx
@@ -8,6 +8,7 @@ import Head from "next/head";
 import favicon from "./favicon.ico"
 import { createRegistration } from "../lib/registration";
 import { useRouter } from 'next/router';
+import { client } from "../lib/client"
 // import toast, { Toaster } from 'react-hot-toast';
 import cogoToast from 'cogo-toast';
 
@@ -23,14 +24,27 @@ export default function Register() {
 
     const [nameCheck, setnameCheck] = useState(false);
     const [phoneCheck, setphoneCheck] = useState(false);
-
-
+   
     // Data Storing in State
     const [name, setNameData] = useState("");
     const [email, setEmailData] = useState("");
     const [password, setPassData] = useState("");
     const [phone, setphoneData] = useState("");
 
+
+
+     //sanity search query with the help of EmailData
+     const query = `*[_type == "user" && email == "${email}"]`;
+     const [registration, setRegistration] = useState([]);
+     useEffect(() => {
+         client.fetch(query).then((registration) => {
+             setRegistration(registration);
+         });
+     }, [query]);
+     console.log('registrations', registration[0])
+     console.log(registration.length)
+
+     
     // email validation
 
     function isEmptyName(event) {
@@ -57,8 +71,17 @@ export default function Register() {
             setPasswrdStyle("block");
         }
     }
-
+    const [phoneLen, setPhoneLen] = useState(true);
+    
     function isPhone(event) {
+        if(event.target.length<1 || event.target.value.length==10)
+        {
+            setPhoneLen(true);
+        }
+        else
+        {
+            setPhoneLen(false)
+        }
         if (event.target.value != "") {
             setphoneData(event.target.
                 value)
@@ -110,8 +133,15 @@ export default function Register() {
         else {
             document.getElementsByClassName("email2")[0].style.display = 'block';
         }
+        if(phoneLen==true)
+        {
+            document.getElementById("warning").style.display = 'none';
+        }
+        else{
+            document.getElementById("warning").style.display = 'block';
+        }
 
-    }, [emailStyle, passwordStyle, phoneCheck, nameCheck]);
+    }, [emailStyle, passwordStyle, phoneCheck, nameCheck,phoneLen]);
 
     const defaultOptions = {
         loop: true,
@@ -129,8 +159,14 @@ export default function Register() {
         },[FormData]
       )
       
-    const handleSubmit = async (e) => {        
-        if (name != "" && email != "" && phone.length == 10 && password != "" && agreeCheck != false) {
+    const handleSubmit = async (e) => {   
+        if(registration.length>0)
+        {
+            e.stopPropagation();
+            e.preventDefault();
+            cogoToast.error("Email Alredy Exists")
+        }     
+        else if (name != "" && email != "" && phone.length == 10 && password != "" && agreeCheck != false) {
             setFormData({ name, email, password, phone })
             // console.log(FormData);
             cogoToast.success("Registered Successfully");
@@ -216,13 +252,9 @@ export default function Register() {
                         <span className={css.icon}>
                         </span>
                         <input id="phone" type="text" onChange={isPhone} required />
-                        <label className="phone" style={{}}>Phone Number</label>
+                        <label className="phone" >Phone Number</label>
                     </div>
-
-                    {/* <div className={css.rememberforgot}>
-                        <label><input type="checkbox" /> Remember me</label>
-                        <a href="#">Forgot Password</a>
-                    </div> */}
+                    <p className={css.warningMsg} id="warning" style={{display: 'none'}}>Enter Valid Phone Number</p>
                     <input type="checkbox" id="agree" name="agree" onChange={isAgree} required></input>&nbsp;Confirm you want to register<br></br>
                     <button style={{ marginTop: '1rem' }} className={css.loginButton} onClick={handleSubmit} type="submit">Register</button>
 
