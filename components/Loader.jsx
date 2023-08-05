4a624914e158f3d2e43676fbb947be775895a2e3
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-08-05 17:13:23 +0530

    initial commit

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -1,9 +1,9 @@
-// import PacmanLoader from "react-spinners/PacmanLoader";
-// import css from "../styles/Login.module.css"
-export default function () {
+import PacmanLoader from "react-spinners/PacmanLoader";
+import css from "../styles/Login.module.css"
+export default function Loader() {
     return (
-        <div className="">
-            {/* <PacmanLoader
+        <div className={css.loaderContainer}>
+            <PacmanLoader
                 className={css.loader}
                 color={"#F54748     "}
                 // loading={loader}
@@ -11,7 +11,7 @@ export default function () {
                 size={30}
                 aria-label="Loading Spinner"
                 data-testid="loader"
-            /> */}
+            />
             Loading
         </div>
     )
