67fc18a77806701093132784868a7f3fb55be378
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-08-05 17:09:26 +0530

    initial commit

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -1,17 +1,17 @@
-// // import PacmanLoader from "react-spinners/PacmanLoader";
-// import css from "../styles/Login.module.css"
-// export default function () {
-//     return (
-//         <div className={css.LoaderContainer}>
-//             {/* <PacmanLoader
-//                 className={css.loader}
-//                 color={"#F54748     "}
-//                 // loading={loader}
-//                 // cssOverride={override}
-//                 size={30}
-//                 aria-label="Loading Spinner"
-//                 data-testid="loader"
-//             /> */}
-//         </div>
-//     )
-// }
+// import PacmanLoader from "react-spinners/PacmanLoader";
+import css from "../styles/Login.module.css"
+export default function () {
+    return (
+        <div className={css.LoaderContainer}>
+            {/* <PacmanLoader
+                className={css.loader}
+                color={"#F54748     "}
+                // loading={loader}
+                // cssOverride={override}
+                size={30}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+            /> */}
+        </div>
+    )
+}
